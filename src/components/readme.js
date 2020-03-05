b43db2e920a26f553e22bc0f03a2090e3f9eabbf
gang.yi <gang.yi@tellyes.com>
2020-03-05 16:03:04 +0800

    test part submit

--- src/components/readme.js
+++ src/components/readme.js
@@ -13,3 +13,16 @@ var instance = new constructor({
 })
 
 // 1.vue定义组件有哪几种方式 说一说各自的不同 有没有写过比较复杂的组件，说一下你用得比较多的技术点，在其中遇到过什么问题没有，然后说一下解决思路?
+
+// render 函数构建
+
+export default {
+  data () {
+    return {
+      name: "王大锤"
+    }
+  },
+  render (h) {
+    return h('span', '你瞅啥呢')
+  }
+}
